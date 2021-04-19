2f219a54db777f2169b2af32aa1535ebd4bcb362
streikerpz <78185251+streikerpz@users.noreply.github.com>
2021-04-19 16:42:36 -0600

    Add files via upload

---
+++ MiPrimeraSolucion/Models/Territory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MiPrimeraSolucion.Models
+{
+    public partial class Territory
+    {
+        public Territory()
+        {
+            EmployeeTerritories = new HashSet<EmployeeTerritory>();
+        }
+
+        public string TerritoryId { get; set; }
+        public string TerritoryDescription { get; set; }
+        public int RegionId { get; set; }
+
+        public virtual ICollection<EmployeeTerritory> EmployeeTerritories { get; set; }
+    }
+}
